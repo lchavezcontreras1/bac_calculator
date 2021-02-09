01eaf6c43bc2bd7ff8c890fd074832b2147f6566
Lesli Chavez Contreras <lchavezcontreras1@my.wctc.edu>
2021-02-08 20:59:58 -0600

    I think I fixed the jquery

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
 $(document).ready(
     function () {
         $("button").click(calculateBac);
-    }
-)
-let beer = parseFloat($("input#beer").val());
-let totalBeer = beer * 0.54;
-$("p#beerounces").text(`${totalBeer} ounces of alcohol.`);
-let wine = parseFloat($("input#wine").val());
-let totalWine = wine * 0.6;
-$("p#wineounces").text(`${totalWine} ounces of alcohol.`);
-let shots = parseFloat($("input#shots").val());
-let totalShots = shots * 0.6;
-$("p#shotsounces").text(`${totalShots} ounces of alcohol.`);
-let weight = parseFloat($("input#weight").val());
-let hours = parseFloat($("input#hours").val());
-function calculateBac() {
-    let bac = ((totalBeer+totalWine+totalShots)*7.5)/ weight;
-    let totalBac = bac - (0.015 * hours);
-    return(totalBac.toFixed(3));
+    });
+
+function calculateBac(e) {
+    e.preventDefault();
+    let totalBeer = parseFloat($("input#beer").val()) * 0.54;
+    let totalWine = parseFloat($("input#wine").val()) * 0.6;
+    let totalShots = parseFloat($("input#shots").val()) * 0.6;
+    $("p#totalbeer").text(`${totalBeer.toFixed(0)} ounces of alcohol.`);
+    $("p#totalwine").text(`${totalWine.toFixed(0)} ounces of alcohol.`);
+    $("p#totalshots").text(`${totalShots.toFixed(0)} ounces of alcohol.`);
+    let totalAlcohol = totalBeer + totalWine + totalShots;
+    let hours = parseFloat($("input#hours").val()) * 0.015;
+    let weight = parseFloat($("input#weight").val());
+    let bac = ((totalAlcohol * 7.5) / weight) - hours;
+    $("p#totalbac").text(`Your Blood Alcohol Content(BAC) is ${bac.toFixed(3)}.`);
+
 }
-$("p#totalbac").text(`Your Blood Alcohol Content (BAC) is ${calculateBac}%`);
